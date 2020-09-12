38323a87a735dfb517941b97bc2fc29fe031aaf4
Bart Sturm <bart@tutormundi.com>
2020-09-12 11:17:20 -0300

    Use nonce

--- server/main.js
+++ server/main.js
@@ -1,6 +1,8 @@
 import { Meteor } from "meteor/meteor";
 import { onPageLoad } from "meteor/server-render";
 
+WebAppInternals.setInlineScriptsAllowed(false);
+
 Meteor.startup(() => {
   // Code to run on server startup.
   console.log(`Greetings from ${module.id}!`);
